fdbcf209e357185f6a1fab2fd526b9478d4899c8
Immo Landwerth <immo@landwerth.net>
2009-03-02 08:36:32 +0000

    Update build logic to reflect move to TFS
    Take care of the following items:
    
    - Add custom build task to remove TFS bindings
    - Remove the .svn references from the build

---
+++ Source/XsdDocumentation.Build/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Clone Detective Build Components")]
+[assembly: AssemblyDescription("Contains MSBuild tasks for Clone Detective")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
