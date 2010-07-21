4ca1c8fc35a4fe1fe752b8377889065b59aa026c
Immo Landwerth <immo@landwerth.net>
2010-07-21 20:15:02 +0000

    Migrate project to SHFB 1.9.1.0

--- Main/Source/CommonAssemblyInfo.cs
+++ Main/Source/CommonAssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("1.8.0.3")]
-[assembly: AssemblyFileVersion("1.8.0.3")]
+[assembly: AssemblyVersion("1.9.1.0")]
+[assembly: AssemblyFileVersion("1.9.1.0")]
