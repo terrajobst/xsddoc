b0cfaa37ff0b4f1b3dc4a30fef6826dda4215e60
Immo Landwerth <immo@landwerth.net>
2012-05-26 11:17:18 -0700

    Fix bootstrapping issues
    See http://shfb.codeplex.com/workitem/32773 for details.

--- Build/Build.bat
+++ Build/Build.bat
@@ -1,5 +1,9 @@
 @echo off
 
+:: Workaround: MSBuild apparently snapshots the variables. Setting the environment
+::             variable from within the build process doesn't work.
+set SHFBCOMPONENTROOT=%~dp0..\Output\Bin
+
 "%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" %~dp0\Build.proj /t:Build
 
 PAUSE
