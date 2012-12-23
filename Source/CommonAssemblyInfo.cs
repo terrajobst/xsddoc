f7c3b7b5adf1b9f795f9c88c38bfab0a4d957ee7
Immo Landwerth <immo@landwerth.net>
2012-12-22 19:25:55 -0800

    Fix version numbers

--- Source/CommonAssemblyInfo.cs
+++ Source/CommonAssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("1.9.5.0")]
-[assembly: AssemblyFileVersion("1.9.5.0")]
+[assembly: AssemblyVersion("1.9.6.0")]
+[assembly: AssemblyFileVersion("1.9.6.0")]
