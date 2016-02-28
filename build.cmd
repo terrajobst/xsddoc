a76a67db3f8290bf586a77313542702311f74e16
Immo Landwerth <immo@landwerth.net>
2016-02-27 16:27:35 -0800

    Use MSBuild 12
    That's the build engine SHFB is compatible with.

--- build.cmd
+++ build.cmd
@@ -3,4 +3,4 @@
 :: Note: We're not using parallel build (/m) because it doesn't seem to
 ::       work with Sandcastle Help File Builder.
 
-"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" %~dp0\build.proj /nologo /v:m /flp:verbosity=detailed %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" %~dp0\build.proj /nologo /v:m /flp:verbosity=detailed %1 %2 %3 %4 %5 %6 %7 %8 %9
