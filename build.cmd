5a1f6eb3a3c4c05e0f9af0b2f2649452b38c8734
Immo Landwerth <immo@landwerth.net>
2016-02-27 16:11:23 -0800

    Move msbuild.log to bin folder
    All our build outputs should should go under bin. This makes CI drops
    a tiny bit easier.

--- build.cmd
+++ build.cmd
@@ -1,6 +1,8 @@
 @ECHO OFF
 
+if not exist bin mkdir bin
+
 :: Note: We're not using parallel build (/m) because it doesn't seem to
 ::       work with Sandcastle Help File Builder.
 
-"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" %~dp0\build.proj /nologo /v:m /flp:verbosity=detailed  %*
+"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" /nologo /m /v:m /nr:false /flp:verbosity=normal;LogFile=bin\msbuild.log %*
