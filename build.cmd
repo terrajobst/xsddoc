665ef6ec91e43d2d8ff986de072e7d3146a82f77
EWSoftware <Eric@EWoodruff.us>
2021-07-05 09:21:05 -0700

    Updated for use with SHFB 2021.4.9.0
    - Updated projects to target .NET 4.7.2.
    - Replaced MSBuild references with equivalent MSBuild NuGet packages.
    - Updated NuGet.exe URL in build.proj to get the latest release.
    - Updaetd MSBuild path to 2019 Community version in build.cmd.

--- build.cmd
+++ build.cmd
@@ -5,4 +5,4 @@ if not exist bin mkdir bin
 :: Note: We're not using parallel build (/m) because it doesn't seem to
 ::       work with Sandcastle Help File Builder.
 
-"%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" /nologo /m /v:m /nr:false /flp:verbosity=normal;LogFile=bin\msbuild.log %*
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" /t:restore;build /nologo /m /v:m /nr:false /flp:verbosity=normal;LogFile=bin\msbuild.log %*
